b341c380b6c97b1c971f2a1b3e737391f9280c8b
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-12-20 16:35:52 +0000

    Add server test and update dependencies [skip ci]

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Hello my friend - Update 2!");
+  res.send("Cheers! God bless you!");
 });
 
 const port = 3000;
---
+++ tests/index.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Cheers! God bless you!");
+});
+
+describe('GET /', () => {
+  it('should return 200 and the correct message', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("Cheers! God bless you!");
+  });
+});
