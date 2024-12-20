b4596681f2b667a3c2c1e8066dbf5cd39a7244f9
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-12-20 16:50:51 +0000

    Modify index.test.js to intentionally fail tests for workflow validation

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Cheers! God bless you!");
+  res.send("Cheers!");
 });
 
 describe('GET /', () => {
